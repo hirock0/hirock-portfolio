60fbf0eaa7d845298e337fcacff5b0384e7723aa
hirock0 <hirockdutta0@gmail.com>
2024-06-28 20:52:41 +0600

    next_auth_token changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const token = request.cookies.get("token")?.value || "";
-  const authToken = request.cookies.get("next-auth.session-token");
+  const authToken = request.cookies.get("__Secure-next-auth.session-token");
   const isPublicPath = path == "/user/login" || path == "/user/signup";
   const tokenExistance = token.length !== 0 || authToken !== undefined;
   if (tokenExistance && isPublicPath) {
